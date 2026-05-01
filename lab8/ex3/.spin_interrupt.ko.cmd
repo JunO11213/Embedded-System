c4d8f41c078e3792726f58dca6be68e8a8a988b2
JunO1213 <june109903@naver.com>
2026-05-01 12:53:57 +0900

    make clean

--- lab8/ex3/.spin_interrupt.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/june/class/lab8/ex3/spin_interrupt.ko := /usr/bin/arm-linux-gnueabi-ld -r -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/june/class/lab8/ex3/spin_interrupt.ko /home/june/class/lab8/ex3/spin_interrupt.o /home/june/class/lab8/ex3/spin_interrupt.mod.o ;  true
--- lab8/ex3/.tmp_versions/spin_interrupt.mod
+++
@@ -1,3 +0,0 @@
-/home/june/class/lab8/ex3/spin_interrupt.ko
-/home/june/class/lab8/ex3/spin_interrupt.o
-
